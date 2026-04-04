a1f4dbddb339e22f1b8847d0d1a6e3849a86306f
zrzr <seapara.zerozero.00@gmail.com>
2026-04-04 23:01:59 +0900

    fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,6 +7,6 @@ async def root():
     return {"message": "Hello World"}    
     
 @app.get("/gemini")
-async def make_response(input: str):
+async def make_gemini_response(input: str):
     response = make_response(input)
     return {"response": response}    
--- backend/app/services/trainer.py
+++ backend/app/services/trainer.py
@@ -3,16 +3,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def make_response(input: str) -> str:
+def make_response(prompt: str) -> str:
     client = genai.Client()
     
     response = client.models.generate_content(
         model="gemini-3-flash-preview",
-        contents=[input],
+        contents=[prompt],
     )
     
-    print(response.text)
-    
-    
+    return response.text
+
 if __name__ == "__main__":
     make_response("What is the best way to lose weight?")
